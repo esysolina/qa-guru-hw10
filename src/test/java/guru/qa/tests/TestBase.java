740f26a2e2aca3f7a57422fc5773fc87287b7b39
esysolina <evelina.ova@gmail.com>
2022-02-06 22:29:20 +0500

    update testbase

--- src/test/java/guru/qa/tests/TestBase.java
+++ src/test/java/guru/qa/tests/TestBase.java
@@ -14,25 +14,22 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        String browser = System.getProperty("browser", "chrome");
-        String version = System.getProperty("version", "91");
-
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = browser;
-        Configuration.browserVersion = version;
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("version", "91");
 
         String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub");
         String login = System.getProperty("login", "user1");
         String pass = System.getProperty("pass", "1234");
 
         String url = "https://" + login + ":" + pass + "@" + remoteUrl;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
